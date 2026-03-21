346d2d1d1cfa7d6241291e2724c2adbb2ae82cd3
rakesh <014rakeshofficial@gmail.com>
2026-03-21 14:27:29 +0530

    Comment controllers syntax is created.

---
+++ Server/src/models/comment.controller.js
@@ -0,0 +1,15 @@
+import { Asynchandler } from "../utils/Asynchandler.js";
+import { Apierror } from "../utils/Apierror.jsr";
+import { Apiresponse } from "../utils/Apiresponse.js";
+import {User} from "../models/user.models.js"
+import {Post} from "../models/post.models.js"
+
+const userComment = Asynchandler(async (req,res) => {
+
+
+})
+
+export {
+    userComment,
+}
+
