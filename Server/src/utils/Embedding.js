3c86721fed6a02423a9519e1d7d2d81fee49d2e9
rakesh <014rakeshofficial@gmail.com>
2026-03-26 20:59:43 +0530

    Updated Embedding.js for better fault handling.

--- Server/src/utils/Embedding.js
+++ Server/src/utils/Embedding.js
@@ -1,18 +1,25 @@
 import { InferenceClient } from "@huggingface/inference";
-
+import {Apierror} from "../utils/Apierror.js"
 const client = new InferenceClient(process.env.HF_TOKEN) ;
 
 try {
-   const generateEmbedding = async (query)=>{
+   const generateEmbedding = async (text)=>{
+    
+    if(!text){
+      return null;
+    }
+
     const output = await client.featureExtraction({
       model: "sentence-transformers/all-MiniLM-L6-v2", 
       inputs: text,
     });
 
-    return output;
+    return Array.isArray(output) ? output.flat() : output;
    }  
 } 
 catch (error) {
     console.error("Hugging Face Embedding Error:", error);
-    return null;
+    throw new Apierror(404,"vector embedding is not generated")
 }
+
+export {generateEmbedding}
