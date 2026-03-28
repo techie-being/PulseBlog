eb1f9709e616ccf087f5a0bdb0e38fbaf5e65cba
rakesh <014rakeshofficial@gmail.com>
2026-03-28 18:46:07 +0530

    Fixed scope issues in Embedding.js

--- Server/src/utils/Embedding.js
+++ Server/src/utils/Embedding.js
@@ -2,24 +2,27 @@ import { InferenceClient } from "@huggingface/inference";
 import {Apierror} from "../utils/Apierror.js"
 const client = new InferenceClient(process.env.HF_TOKEN) ;
 
-try {
-   const generateEmbedding = async (text)=>{
+
+const generateEmbedding = async (text)=>{
     
-    if(!text){
-      return null;
-    }
+if(!text){
+  return null;
+}
 
-    const output = await client.featureExtraction({
-      model: "sentence-transformers/all-MiniLM-L6-v2", 
-      inputs: text,
-    });
+try {
+  const output = await client.featureExtraction({
+    model: "sentence-transformers/all-MiniLM-L6-v2", 
+      nputs: text,
+  });
 
-    return Array.isArray(output) ? output.flat() : output;
-   }  
-} 
+  return Array.isArray(output) ? output.flat() : output;
+  
+}
+
 catch (error) {
     console.error("Hugging Face Embedding Error:", error);
     throw new Apierror(404,"vector embedding is not generated")
+ };
+ 
 }
-
 export {generateEmbedding}
