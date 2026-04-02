47ad3d731d6f167880a7576e0aa6c4ebc3691897
rakesh <014rakeshofficial@gmail.com>
2026-04-02 19:33:26 +0530

    implemented standard Image Size  and File Filter.

--- Server/src/middlewares/multer.middlewares.js
+++ Server/src/middlewares/multer.middlewares.js
@@ -1,12 +1,27 @@
 import multer from "multer";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp')
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
-export const upload = multer({storage})
+export const upload = multer({
+  storage,
+
+  limits: {
+    fileSize: 100 * 1024, // Strictly 100 KB
+  },
+  
+  fileFilter: (req, file, cb) => {
+    // Check if the file is an image
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only images (.jpg, .png, etc.) are allowed!"), false);
+    }
+  },
+});
