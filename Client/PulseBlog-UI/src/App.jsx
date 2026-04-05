085d79c075b979e6a78c6617727846903cce87ac
isidcode <sidharthsharma1410@gmail.com>
2026-04-05 14:40:08 +0530

    update_app.jsx

--- Client/PulseBlog-UI/src/App.jsx
+++ Client/PulseBlog-UI/src/App.jsx
@@ -1,16 +1,26 @@
 import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
+import HomePage from "./pages/Home.page";
+import WritePage from "./pages/write.page";
+import PostDetail from "./pages/PostDetail.page";
 
 const App = () => {
     return (
-        <Routes>
-         <Route path="/" element={<Navbar/>}>
-           <Route path="signin" element={<UserAuthForm type="sign-in"/>}/>
-           <Route path="signup" element={<UserAuthForm type="sign-up"/>}/>
-         </Route>
-        </Routes> 
-           )
-}
+        <>
+            <Toaster position="top-center" />
+            <Routes>
+                <Route path="/" element={<Navbar />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+                    <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+                    <Route path="write"  element={<WritePage />} />
+                    <Route path="post/:postId" element={<PostDetail />} />
+                </Route>
+            </Routes>
+        </>
+    );
+};
 
 export default App;
