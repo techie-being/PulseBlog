edfc88e27efc08a14966460de73b90d4f3ce2f6a
rakesh <014rakeshofficial@gmail.com>
2026-04-18 22:33:15 +0530

    Remved bugs.

--- Server/src/utils/Sendemail.js
+++ Server/src/utils/Sendemail.js
@@ -1,10 +1,14 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
+  console.log("Attempting to send email to:", options.email);
+  console.log("Using Host:", process.env.EMAIL_HOST);
   // 1. Create a transporter
   const transporter = nodemailer.createTransport({
+    service: 'gmail',
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -21,7 +25,7 @@ const sendEmail = async (options) => {
   };
 
   // 3. Actually send the email
-  await transporter.sendMail(mailOptions);
+  return await transporter.sendMail(mailOptions);
 };
 
 export {sendEmail};
