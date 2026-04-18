cc42caafe0374c4a79df30ad16e0986bea0d4f0a
rakesh <014rakeshofficial@gmail.com>
2026-04-18 22:34:12 +0530

    Added username  in passport.js

--- Server/src/Config/Passport.js
+++ Server/src/Config/Passport.js
@@ -1,51 +1,54 @@
 import passport from "passport";
 import { Strategy as googleStrategy } from "passport-google-oauth20";
-import {User} from "../models/user.models.js";
+import { User } from "../models/user.models.js";
 
 passport.use(
-    new googleStrategy(
-        {
-            clientID:process.env.GOOGLE_AUTH_CLIENT_ID,
-            clientSecret:process.env.GOOGLE_AUTH_CLIENT_SECRET,
-            callbackURL:process.env.GOOGLE_CALLBACK_URL
-        },
+  new googleStrategy(
+    {
+      clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
 
-        //this is callback runs immediately after google send user back to App.
-        async (accessToken,refreshToken,profile,done) => {
-            console.log("data sent by the google auth",profile);
+    //this is callback runs immediately after google send user back to App.
+    async (accessToken, refreshToken, profile, done) => {
+      console.log("data sent by the google auth", profile);
 
-            try {
-                const googleId = profile.id;
-                const email = profile.emails[0].value;
-                const name = profile.displayName;
-                const avatar = profile.photos[0].value;
+      try {
+        const googleId = profile.id;
+        const email = profile.emails[0].value;
+        const name = profile.displayName;
+        const avatar = profile.photos[0].value;
 
-                const user = await User.findOne({email:email});
-                 
-                //if user found
-                if(user){
-                    return done(null,user)
-                }
+        const user = await User.findOne({ email: email });
 
-                //user not found
-                else{
-                    await User.create(
-                        {
-                            fullname:name,
-                            email:email,
-                            avatar:avatar,
-                            provider:'google',
-                            providerId:googleId,
-                        }
-                    )
-                    return done(null,user)
-                }
-            } 
-            
-            catch (error) {
-                console.log("something went wrong while using google auth",error);
-                return done(error,null);
-            }
+        //if user found
+        if (user) {
+          return done(null, user);
         }
-    )
-)
+
+        //user not found
+        // user not found
+        else {
+          const newUser = await User.create({
+            // Assign to a variable
+            fullname: name,
+            email: email,
+            avatar: avatar,
+            provider: "google",
+            providerId: googleId,
+            // Don't forget 'username' if your schema requires it!
+            username: email.split("@")[0] + Math.floor(Math.random() * 1000),
+          });
+          return done(null, newUser); // Return the newly created user
+        }
+
+      } 
+      
+      catch (error) {
+        console.log("something went wrong while using google auth", error);
+        return done(error, null);
+      }
+    },
+  ),
+);
