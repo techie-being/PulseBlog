90c0e0a56629517c75895e349579b18f6dee96ba
rakesh <014rakeshofficial@gmail.com>
2026-08-12 11:35:53 +0530

    Implement user authentication initialization in App component with Redux integration

--- Client/PulseBlog-UI/src/App.jsx
+++ Client/PulseBlog-UI/src/App.jsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { Toaster } from "react-hot-toast";
+
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
 import HomePage from "./pages/Home.page";
@@ -11,47 +14,69 @@ import SearchPage from "./pages/Search.page";
 import OnboardingPage from "./pages/Onboarding.page";
 import SettingsPage from "./pages/Settings.page";
 
-// --- NEW AUTH PAGES ---
 import ForgotPassword from "./pages/ForgotPassword.page";
 import ResetPassword from "./pages/ResetPassword.page";
 import LoginSuccess from "./pages/LoginSuccess.page";
 import LoginFailed from "./pages/LoginFailed.page";
 
+import { loginSuccess } from "./redux/slices/authSlice";
+import axiosInstance from "./api/axiosInstance";
+
 const App = () => {
-    return (
-        <>
-            <Toaster position="top-right" />
-            <Routes>
-                <Route path="/" element={<Navbar />}>
-                    <Route index element={<HomePage />} />
-                    
-                    {/* Standard Auth */}
-                    <Route path="signin" element={<UserAuthForm type="sign-in" />} />
-                    <Route path="signup" element={<UserAuthForm type="sign-up" />} />
-                    
-                    {/* Password Recovery */}
-                    <Route path="forgot-password" element={<ForgotPassword />} />
-                    <Route path="reset-password/:token" element={<ResetPassword />} />
+  const dispatch = useDispatch();
 
-                    {/* Google Auth Redirects */}
-                    <Route path="login-success" element={<LoginSuccess />} />
-                    <Route path="login-failed"  element={<LoginFailed />} />
-                    
-                    {/* Editor / Content Creation */}
-                    <Route path="write"        element={<WritePage />} />
-                    <Route path="edit/:postId" element={<WritePage />} />
-                    
-                    {/* Content Viewing & User Data */}
-                    <Route path="post/:postId" element={<PostDetail />} />
-                    <Route path="dashboard"    element={<DashboardPage />} />
-                    <Route path="/user/:username" element={<ProfilePage />} />
-                    <Route path="search" element={<SearchPage />} />
-                    <Route path="onboarding" element={<OnboardingPage />} />
-                    <Route path="settings" element={<SettingsPage />} />
-                </Route>
-            </Routes>
-        </>
-    );
+  useEffect(() => {
+    const initializeAuth = async () => {
+      try {
+        const res = await axiosInstance.get("/users/current-user");
+
+        const user = res.data.data;
+
+        console.log("Before Redux:", user);
+
+        dispatch(loginSuccess(user));
+
+        console.log("Redux loginSuccess dispatched");
+      } catch (error) {
+        console.log("Authentication initialization failed:", error);
+      }
+    };
+
+    initializeAuth();
+  }, [dispatch]);
+  return (
+    <>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<HomePage />} />
+
+          {/* Standard Auth */}
+          <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+          <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+
+          {/* Password Recovery */}
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="reset-password/:token" element={<ResetPassword />} />
+
+          {/* Google Auth Redirects */}
+          <Route path="login-success" element={<LoginSuccess />} />
+          <Route path="login-failed" element={<LoginFailed />} />
+
+          {/* Editor / Content Creation */}
+          <Route path="write" element={<WritePage />} />
+          <Route path="edit/:postId" element={<WritePage />} />
+
+          {/* Content Viewing & User Data */}
+          <Route path="post/:postId" element={<PostDetail />} />
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="/user/:username" element={<ProfilePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="onboarding" element={<OnboardingPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
-
 export default App;
