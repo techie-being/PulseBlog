37bae6f2418f8bc63c2db9df162f78f70437c061
rakesh <014rakeshofficial@gmail.com>
2026-09-12 18:04:26 +0530

    Configures Resend successfully.

--- Server/src/utils/sendEmail.js
+++ Server/src/utils/sendEmail.js
@@ -1,32 +1,19 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
   console.log("Attempting to send email to:", options.email);
-  console.log("Using Host:", process.env.EMAIL_HOST);
-  console.log("Using Port:", process.env.EMAIL_PORT);
-  // 1. Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    
-  });
 
-  // 2. Define email options
-  const mailOptions = {
+  const email = await resend.emails.send({
     from: process.env.EMAIL_FROM,
     to: options.email,
     subject: options.subject,
     text: options.message,
     html: `<h1>Reset Password</h1><p>${options.message}</p>`,
-  };
+  });
 
-  // 3. Actually send the email
-  return await transporter.sendMail(mailOptions);
+  return email;
 };
 
 export { sendEmail };
