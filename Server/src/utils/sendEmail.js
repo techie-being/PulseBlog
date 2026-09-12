3d7d91aa66e44a18de48efd36a677d8ee4998822
rakesh <014rakeshofficial@gmail.com>
2026-09-12 17:30:04 +0530

    Password reset 8

--- Server/src/utils/sendEmail.js
+++ Server/src/utils/sendEmail.js
@@ -8,7 +8,7 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
